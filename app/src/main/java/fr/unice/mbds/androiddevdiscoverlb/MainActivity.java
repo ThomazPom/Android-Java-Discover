9ccf76b0a502b0e8417dbfa71789f3540b9cf713
Clément Lavoisier <clem.lavoisier@hotmail.fr>
2016-10-14 14:53:37 +0200

    Plein de truc jolie :)

--- app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/MainActivity.java
+++ app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.unice.mbds.androiddevdiscoverlb;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickEnregistrer(View v) {
+        Intent intent = new Intent(this, enregitrerActivity.class);
+        startActivity(intent);
+    }
+
+    public void lost_focus_main(View v)
+    {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
--- app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/enregitrerActivity.java
+++ app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/enregitrerActivity.java
@@ -1,7 +1,10 @@
 package fr.unice.mbds.androiddevdiscoverlb;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class enregitrerActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class enregitrerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enregitrer);
+    }
+
+    public void lost_focus_enregistrer(View v)
+    {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
