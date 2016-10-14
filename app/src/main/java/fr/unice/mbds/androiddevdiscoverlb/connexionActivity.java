e2846d10e55d3fca8b2bc9d5f53d25a0301c45a2
Clément Lavoisier <clem.lavoisier@hotmail.fr>
2016-10-14 15:58:16 +0200

    new files and review

--- app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/connexionActivity.java
+++ app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/connexionActivity.java
@@ -1,30 +1,20 @@
 package fr.unice.mbds.androiddevdiscoverlb;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
-public class MainActivity extends AppCompatActivity {
+public class connexionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connexion);
     }
 
-    public void onClickEnregistrer(View v) {
-        Intent intent = new Intent(this, enregitrerActivity.class);
-        startActivity(intent);
-    }
-
-    public void onClickConnexion(View v) {
-        Intent intent = new Intent(this, connexionActivity.class);
-        startActivity(intent);
-    }
-    public void lost_focus_main(View v)
+    public void lost_focus_connexion(View v)
     {
         View view = this.getCurrentFocus();
         if (view != null) {
