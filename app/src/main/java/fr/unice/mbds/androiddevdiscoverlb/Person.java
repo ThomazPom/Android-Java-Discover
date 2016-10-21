d4992897d731ddbdf7d781b3e22f948b3e8299e0
Clément Lavoisier <clem.lavoisier@hotmail.fr>
2016-10-21 14:45:48 +0200

    TP2 WS Début

---
+++ app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/Person.java
@@ -0,0 +1,107 @@
+package fr.unice.mbds.androiddevdiscoverlb;
+
+import java.util.HashMap;
+
+/**
+ * Created by Clem on 21/10/2016.
+ */
+public class Person {
+
+    //public static HashMap<String,HashMap<String,String>> clients = new  HashMap<String,HashMap<String,String>>();
+    private String email;
+    private Person createdBy;
+    private String password;
+    private String nom;
+    private String prenom;
+    private String sexe;
+    private String telephone;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Person getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Person createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+   /*public static void createClientsDefault(){
+
+        addClient("kevin@gmail.com"  ,"LAVOISIER"  , "Clément","Masculin" ,"0650686074","clem" );
+        addClient("vaki@gmail.com","BENHAMOU","Thomas","Masculin","0650686074","Thom");
+
+    }*/
+
+    public Person(String email, String nom, String prenom, String sexe, String tel, String password,Person p){
+
+       /* HashMap<String,String> h = new HashMap<String,String>();
+        h.put("MAIL",mail);
+        h.put("NOM",nom);
+        h.put("PRENOM",prenom);
+        h.put("SEXE",sexe);
+        h.put("TELEPHONE",tel);
+        h.put("PASSWORD",password);*/
+
+        this.nom = nom;
+        this.prenom = prenom;
+        this.sexe = sexe;
+        this.telephone = tel;
+        this.email = email;
+        this.password = password;
+        this.createdBy = p;
+
+
+        // clients.put(mail,h);
+
+    }
+
+}
--- app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/enregitrerActivity.java
+++ app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/enregitrerActivity.java
@@ -1,14 +1,28 @@
 package fr.unice.mbds.androiddevdiscoverlb;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 import utils.ValidateFields;
 
@@ -47,4 +61,75 @@ public class enregitrerActivity extends AppCompatActivity {
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
+    public void enregistrerOnCick(View view){
+
+    }
+
+     abstract class RegisterTask extends AsyncTask<Person,Void,Person> {
+
+         protected Person doInBackground(Person... people) {
+             String url = "http://95.142.161.35:1337/person/";
+             for (int i = 0; i < people.length; i++) {
+                 Person person = people[1];
+                 try {
+                     HttpClient client = new DefaultHttpClient();
+                     HttpPost post = new HttpPost(url);
+
+                     // add header
+
+                     post.setHeader("Content-Type", "application/json");
+                     JSONObject obj = new JSONObject();
+                     obj.put("prenom", person.getPrenom());
+                     obj.put("nom", person.getNom());
+                     obj.put("sexe", person.getSexe());
+                     obj.put("telephone", person.getTelephone());
+                     obj.put("email", person.getEmail());
+                     obj.put("createdby", person.getCreatedBy());
+                     obj.put("password", person.getPassword());
+                     StringEntity entity = new StringEntity(obj.toString());
+
+                     post.setEntity(entity);
+
+                     HttpResponse response = client.execute(post);
+                     System.out.println("\nSending 'POST' request to URL : " + url);
+                     System.out.println("Post parameters : " + post.getEntity());
+                     System.out.println("Response Code : " +
+                             response.getStatusLine().getStatusCode());
+
+                     BufferedReader rd = new BufferedReader(
+                             new InputStreamReader(response.getEntity().getContent()));
+
+                     StringBuffer result = new StringBuffer();
+                     String line = "";
+                     while ((line = rd.readLine()) != null) {
+                         result.append(line);
+                     }
+
+                     System.out.println(result.toString());
+                     return person;
+                 } catch (Exception e) {
+
+                 }
+             }
+             return null;
+         }
+
+         @Override
+         protected void onPreExecute() {
+             super.onPreExecute();
+             //Afficher un loading "Patientez, inscription en cours..."
+         }
+
+         @Override
+         protected void onPostExecute(Person person) {
+             super.onPostExecute(person);
+             //Enlever le loading
+             //Traiter la person
+             if (person != null) {
+             } else {
+             }
+             //Renvoyer vers le login
+             //Fermer l'activité Enregistrer
+         }
+     }
 }
