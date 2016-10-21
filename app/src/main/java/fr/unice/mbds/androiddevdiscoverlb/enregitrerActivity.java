db5f4e524f5c8b0ef55fb6f91a11a358d8313966
Clément Lavoisier <clem.lavoisier@hotmail.fr>
2016-10-21 15:50:09 +0200

    finish enregistrer

--- app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/enregitrerActivity.java
+++ app/src/main/java/fr/unice/mbds/androiddevdiscoverlb/enregitrerActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -25,9 +24,6 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 import utils.ValidateFields;
 
 public class enregitrerActivity extends AppCompatActivity {
@@ -48,7 +44,7 @@ public class enregitrerActivity extends AppCompatActivity {
 
         lockviews.add(this.findViewById(R.id.Save));
 
-        ValidateFields validator = new ValidateFields(this,lockviews,true);
+        ValidateFields validator = new ValidateFields(this, lockviews, true);
         validator.verifyOnFocusChangeListener(this.findViewById(R.id.editTextNom));
         validator.verifyOnFocusChangeListener(this.findViewById(R.id.editTextPrenom));
         validator.verifyOnFocusChangeListener(this.findViewById(R.id.editTextTelephone));
@@ -57,15 +53,15 @@ public class enregitrerActivity extends AppCompatActivity {
         validator.verifyOnFocusChangeListener(mdp2);
     }
 
-    public void lost_focus_enregistrer(View v)
-    {
+    public void lost_focus_enregistrer(View v) {
         View view = this.getCurrentFocus();
         if (view != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
-    public void enregistrerOnCick(View view){
+
+    public void enregistrerOnCick(View view) {
         String sexe = "";
 
         sexe = ((RadioButton) enregitrerActivity.this.findViewById(((RadioGroup) enregitrerActivity.this.findViewById(R.id.radiogroupsexe)).getCheckedRadioButtonId())).getText().toString();
@@ -78,17 +74,19 @@ public class enregitrerActivity extends AppCompatActivity {
                 sexe,
                 ((EditText) enregitrerActivity.this.findViewById(R.id.editTextTelephone)).getText().toString(),
                 ((EditText) enregitrerActivity.this.findViewById(R.id.editTextMDP1)).getText().toString()
-                ,null);
+                , null);
 
         Person[] ctab = new Person[1];
         ctab[0] = c1;
 
         new RegisterTask().execute(ctab);
     }
+
     ProgressDialog progressDialog;
+
     public void showProgressDialog(boolean isVisible) {
         if (isVisible) {
-            if(progressDialog==null) {
+            if (progressDialog == null) {
                 progressDialog = new ProgressDialog(this);
                 progressDialog.setMessage(this.getResources().getString(R.string.please_wait));
                 progressDialog.setCancelable(false);
@@ -101,68 +99,66 @@ public class enregitrerActivity extends AppCompatActivity {
                 });
                 progressDialog.show();
             }
-        }
-        else {
-            if(progressDialog!=null) {
+        } else {
+            if (progressDialog != null) {
                 progressDialog.dismiss();
             }
         }
     }
 
-    class RegisterTask extends AsyncTask<Person,Void,Person> {
+    class RegisterTask extends AsyncTask<Person, Void, Person> {
 
-         protected Person doInBackground(Person... people) {
-             String url = "http://95.142.161.35:1337/person/";
-             for (int i = 0; i < people.length; i++) {
-                 Person person = people[1];
-                 try {
-                     HttpClient client = new DefaultHttpClient();
-                     HttpPost post = new HttpPost(url);
+        @Override
+        protected Person doInBackground(Person... people) {
+            String url = "http://95.142.161.35:1337/person/";
+                Person person = people[0];
+                try {
+                    HttpClient client = new DefaultHttpClient();
+                    HttpPost post = new HttpPost(url);
 
-                     // add header
+                    // add header
 
-                     post.setHeader("Content-Type", "application/json");
-                     JSONObject obj = new JSONObject();
-                     obj.put("prenom", person.getPrenom());
-                     obj.put("nom", person.getNom());
-                     obj.put("sexe", person.getSexe());
-                     obj.put("telephone", person.getTelephone());
-                     obj.put("email", person.getEmail());
-                     obj.put("createdby", person.getCreatedBy());
-                     obj.put("password", person.getPassword());
-                     StringEntity entity = new StringEntity(obj.toString());
+                    post.setHeader("Content-Type", "application/json");
+                    JSONObject obj = new JSONObject();
+                    obj.put("nom", person.getNom());
+                    obj.put("prenom", person.getPrenom());
+                    obj.put("sexe", person.getSexe());
+                    obj.put("telephone", person.getTelephone());
+                    obj.put("email", person.getEmail());
+                    obj.put("createdby", person.getCreatedBy());
+                    obj.put("password", person.getPassword());
+                    StringEntity entity = new StringEntity(obj.toString());
 
-                     post.setEntity(entity);
+                    post.setEntity(entity);
+                    post.addHeader("content-type","application/json");
+                    HttpResponse response = client.execute(post);
+                    System.out.println("\nSending 'POST' request to URL : " + url);
+                    System.out.println("Post parameters : " + post.getEntity());
+                    System.out.println("Response Code : " +
+                            response.getStatusLine().getStatusCode());
 
-                     HttpResponse response = client.execute(post);
-                     System.out.println("\nSending 'POST' request to URL : " + url);
-                     System.out.println("Post parameters : " + post.getEntity());
-                     System.out.println("Response Code : " +
-                             response.getStatusLine().getStatusCode());
+                    BufferedReader rd = new BufferedReader(
+                            new InputStreamReader(response.getEntity().getContent()));
 
-                     BufferedReader rd = new BufferedReader(
-                             new InputStreamReader(response.getEntity().getContent()));
+                    StringBuffer result = new StringBuffer();
+                    String line = "";
+                    while ((line = rd.readLine()) != null) {
+                        result.append(line);
+                    }
 
-                     StringBuffer result = new StringBuffer();
-                     String line = "";
-                     while ((line = rd.readLine()) != null) {
-                         result.append(line);
-                     }
-
-                     System.out.println(result.toString());
-                     return person;
-                 } catch (Exception e) {
-
-                 }
-             }
-             return null;
-         }
+                    System.out.println(result.toString());
+                    return person;
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            return null;
+        }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             showProgressDialog(true);
-            Toast.makeText(enregitrerActivity.this, R.string.inscription_ok, Toast.LENGTH_LONG).show();
+            Toast.makeText(enregitrerActivity.this, R.string.please_wait, Toast.LENGTH_LONG).show();
         }
 
         @Override
@@ -171,14 +167,14 @@ public class enregitrerActivity extends AppCompatActivity {
             //enlever le loading
             showProgressDialog(false);
             //Enlever la person
-            if(person!= null){
-                Intent i= new Intent(enregitrerActivity.this, connexionActivity.class);
+            if (person != null) {
+                Intent i = new Intent(enregitrerActivity.this, connexionActivity.class);
                 startActivity(i);
 
-            }else {
+            } else {
                 Toast.makeText(enregitrerActivity.this, R.string.ErreurRegister, Toast.LENGTH_LONG).show();
             }
         }
-     }
+    }
 
 }
